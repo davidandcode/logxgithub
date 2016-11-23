e52acdc260bbbb613e861b24aba91db5dbb07d7b
Shengwei Wang <Shengwei.Wang@creditkarma.com>
2016-11-23 09:59:17 -0800

    adding prod issue sol'n

---
+++ src/main/scala/com/creditkarma/logx/example/OldTest.scala
@@ -0,0 +1,157 @@
+package com.creditkarma.logx.example
+
+/**
+  * Created by shengwei.wang on 11/21/16.
+  */
+
+import java.io.{BufferedReader, FileReader}
+
+import com.creditkarma.logx.base.{BufferedData, CheckpointService, Transformer, Writer, _}
+import com.creditkarma.logx.impl.checkpoint.KafkaCheckpoint
+import com.creditkarma.logx.impl.checkpointservice.KafkaCheckpointService
+import com.creditkarma.logx.impl.streambuffer.SparkRDD
+import com.creditkarma.logx.impl.streamreader.KafkaSparkRDDReader
+import com.creditkarma.logx.instrumentation.LogInfoInstrumentor
+import com.creditkarma.logx.utils.gcs.{ZKCreate, ZooKeeperConnection}
+import info.batey.kafka.unit.KafkaUnit
+import org.apache.kafka.clients.consumer.{ConsumerRecord, KafkaConsumer, OffsetAndMetadata}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.streaming.kafka010.OffsetRange
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.zookeeper.ZooKeeper
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
+
+/**
+  * Created by yongjia.wang on 11/17/16.
+  */
+object OldTest {
+
+  class IdentityTransformer[I <: BufferedData] extends Transformer[I, I]{
+    override def transform(input: I): I = input
+  }
+
+
+
+
+
+
+  val (zkPort, kafkaPort) = (5556, 5558)
+
+  object TestKafkaServer {
+
+    val kafkaUnitServer = new KafkaUnit(zkPort, kafkaPort)
+    val kp = new KafkaProducer[String, String](kafkaParams.asJava)
+
+    def start(): Unit = {
+      kafkaUnitServer.startup()
+      kafkaUnitServer.createTopic("testTopic", 5)
+    }
+
+    val testData: ListBuffer[String] = ListBuffer.empty
+    var dataSentToKafka = 0
+
+    def loadData (dataFile: String): Unit = {
+      val br = new BufferedReader(new FileReader(dataFile))
+      while({
+        val line = br.readLine()
+        if(line != null){
+          testData.append(line)
+          true
+        }
+        else{
+          false
+        }
+      }){
+      }
+    }
+
+    def sendNextMessage(n: Int): Unit = {
+      var i = 0
+      while(i < n && sendNextMessage()){
+        i += 1
+      }
+    }
+
+    def sendNextMessage(): Boolean = {
+      if(dataSentToKafka == testData.size){
+        false
+      }
+      else {
+        kp.send(new ProducerRecord[String, String]("testTopic", null, testData(dataSentToKafka)))
+        dataSentToKafka += 1
+        true
+      }
+    }
+
+    def sentMessages: Seq[String] = testData.slice(0, dataSentToKafka)
+
+    def stop(): Unit = {
+      kp.close()
+      kafkaUnitServer.shutdown()
+    }
+  }
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> s"localhost:$kafkaPort",
+    "key.serializer" -> classOf[StringSerializer],
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.serializer" -> classOf[StringSerializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "test"
+  )
+
+  def main(args: Array[String]): Unit = {
+
+    try{
+
+      TestKafkaServer.start()
+      TestKafkaServer.loadData("test_data/KRSOffer.json")
+
+      LogManager.getLogger("org.apache").setLevel(Level.WARN)
+      LogManager.getLogger("kafka").setLevel(Level.WARN)
+
+
+      SparkContext.getOrCreate(new SparkConf().setAppName("test").setMaster("local[2]")
+        .set("spark.driver.host", "127.0.0.1")
+        // set local host explicitly, the call through java.net.InetAddress.getLocalHost on laptop with VPN can be inconsistent
+        // Also if it returns IPV6, Spark won't work with it
+      )
+
+
+
+      //val conn: ZooKeeperConnection = new ZooKeeperConnection();
+      //val zk:ZooKeeper = conn.connect("localhost:" + 5556);
+
+
+
+    } catch{
+      case e: Exception => e.printStackTrace
+    } finally{
+      TestKafkaServer.stop()
+    }
+
+  }
+
+  def loadDataToLocalKafka(kafkaUnitServer: KafkaUnit) = {
+    kafkaUnitServer.startup()
+
+    val kp = new KafkaProducer[String, String](kafkaParams.asJava)
+    val br = new BufferedReader(new FileReader("test_data/KRSOffer.json"))
+    while({
+      val line = br.readLine()
+      kp.send(new ProducerRecord[String, String]("testTopic", null, line))
+      kp.flush()
+      line != null
+    }){
+    }
+
+    kp.close()
+  }
+}
---
+++ src/main/scala/com/creditkarma/logx/example/ProdIssue.scala
@@ -0,0 +1,169 @@
+package com.creditkarma.logx.example
+
+/**
+  * Created by shengwei.wang on 11/22/16.
+  */
+import java.io.{ByteArrayInputStream, SequenceInputStream}
+
+import com.creditkarma.logx.utils.gcs.{GCSUtils, WriteUtil}
+import com.google.api.client.http.InputStreamContent
+import com.google.api.services.storage.model.StorageObject
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import java.io._
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+import com.google.api.services.storage.Storage
+
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+import org.apache.spark.streaming.kafka010.{KafkaUtils, OffsetRange}
+import org.apache.kafka.common.TopicPartition
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.services.storage.model.StorageObject
+import com.google.api.client.http.InputStreamContent
+import java.io._;
+
+
+object SparkForProd {
+
+  def main(args: Array[String]) {
+
+    // Create a Scala Spark Context.
+    val conf = new SparkConf().setAppName("spark gcs connector test").setMaster("local[8]")
+    val sc = new SparkContext(conf)
+
+    sc.hadoopConfiguration.set("fs.gs.project.id", "295779567055")
+    sc.hadoopConfiguration.set("fs.gs.impl","com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+    sc.hadoopConfiguration.set("google.cloud.auth.service.account.enable", "true")
+    sc.hadoopConfiguration.set("google.cloud.auth.service.account.email", "dataeng-test@modular-shard-92519.iam.gserviceaccount.com")
+    sc.hadoopConfiguration.set("google.cloud.auth.service.account.keyfile", "/Users/shengwei.wang/projects/DataScience-ac040bae47fb.p12")
+
+
+    // this is a read test
+    //Buckets/ck30616327/new/insert/kafka/sponge_OfferVisible/2016/11/20
+    //Buckets/ck30616327/new/insert/kafka/sponge_OfferVisible/2016/11/21
+    val myRDD = sc.textFile("gs://ck30616327/new/insert/kafka/sponge_OfferVisible/2016/11/21/daily_composed_1479728392374_AqVyAz_50.json")
+    //myRDD.take(100).foreach(myString => println("-----------"+myString+"--------"))
+
+myRDD.foreach(tempString =>{
+if(tempString.contains("loanRate")) {
+  //println(" ============= > before: " + tempString)
+
+  val after:String =tempString.replaceAll(
+    "(\"loanRate\":)([0-9.]+)(,)","$1"+ "\"" +"$2"+"\"" +","
+
+  )
+
+  //println(" ============= > after: " + after)
+}
+})
+
+    val newRDD = myRDD.map(tempString =>{
+
+     // println(" ============= > all: " + tempString)
+      var after:String = tempString
+
+      if(tempString.contains("loanRate")) {
+       // println(" ============= > before: " + tempString)
+
+        after =tempString.replaceAll(
+          "(\"loanRate\":)([0-9.]+)(,)","$1"+ "\"" +"$2"+"\"" +","
+
+        )
+
+        //println(" ============= > after: " + after)
+      }
+
+      after
+    })
+
+    //myRDD.take(100).foreach(tempString => println(tempString))
+    //newRDD.take(1).foreach(tempString => println(tempString))
+    newRDD.foreach(afterString => {
+      if(afterString.contains("loanRate"))
+      println(afterString)})
+/*
+    val words = myRDD.flatMap(line => line.split(" "))
+    // Transform into pairs and count.
+    val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y} // Save the word count back out to a text file, causing evaluation. counts.saveAsTextFile(outputFile)
+    counts.collect().foreach(println)
+*/
+    // this is a write test
+   // myRDD.saveAsTextFile("gs://dataeng_test/prodissue")
+
+   // newRDD.saveAsObjectFile("/Users/shengwei.wang/Desktop/crap.json")
+    //newRDD.saveAsObjectFile("gs://dataeng_test/prodissue_temp/crap.json")
+
+
+
+
+
+
+
+    ////////
+
+
+
+
+
+
+        val storage = GCSUtils.getService("/Users/shengwei.wang/projects/DataScience-f7d364638ad4.json", 10000, 10000)
+
+        val request = storage.objects.insert(
+          "dataeng_test",
+          new StorageObject().setName("fromkafkatest_clientapi/usingClientAPI" + "bigbigbig.json"),
+          new InputStreamContent("application/json", iteratorToStream(newRDD.toLocalIterator) // don't forget add new line for each string
+        )
+        )
+        request.getMediaHttpUploader.setDirectUploadEnabled(true)
+        request.execute()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /////
+
+
+
+  }
+
+
+  def iteratorToStream (strings: Iterator[String]): InputStream = {
+    new SequenceInputStream({
+      val i = strings map { s => new ByteArrayInputStream(s.getBytes("UTF-8")) }
+      i.asJavaEnumeration
+    })
+  }
+
+
+
+
+
+
+}
